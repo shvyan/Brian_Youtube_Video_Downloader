97fb9f697c3a0c2d2a03bc0150796fd9f980ba93
shvyan <106316952+shvyan@users.noreply.github.com>
2023-06-19 18:21:32 +0300

    Update Video_Downloader.py

--- Video_Downloader.py
+++ Video_Downloader.py
@@ -1,37 +1,25 @@
 import streamlit as st
+import pandas as pd
 from pytube import YouTube
-
-def download_video(url, save_path):
-    try:
-        # Create a YouTube object with the video URL
-        youtube = YouTube(url)
-
-        # Get the highest resolution progressive video
-        video = youtube.streams.get_highest_resolution()
-
-        # Download the video to the specified save path
-        video.download(save_path)
-
-        st.success("Video downloaded successfully!")
-
-    except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
-
+import base64
+from io import BytesIO
 def main():
-    st.title("YouTube Video Downloader")
-
-    # User input: YouTube video URL
-    video_url = st.text_input("Enter YouTube video URL:")
-
-    # User input: Save location
-    save_location = st.text_input("Enter save location:")
-
-    # Download button
-    if st.button("Download"):
-        if video_url and save_location:
-            download_video(video_url, save_location)
-        else:
-            st.warning("Please enter a video URL and save location.")
-
-if __name__ == "__main__":
-    main()
+	path = st.text_input('Enter URL of any youtube video')
+	option = st.selectbox(
+     'Select type of download',
+     ('audio', 'highest_resolution', 'lowest_resolution'))
+	
+	matches = ['audio', 'highest_resolution', 'lowest_resolution']
+	if st.button("download"): 
+		video_object =  YouTube(path)
+		st.write("Title of Video: " + str(video_object.title))
+		st.write("Number of Views: " + str(video_object.views))
+		if option=='audio':
+			video_object.streams.get_audio_only().download() 		#base64.b64encode("if file is too large").decode()	
+		elif option=='highest_resolution':
+			video_object.streams.get_highest_resolution().download()
+		elif option=='lowest_resolution':
+			video_object.streams.get_lowest_resolution().download()
+	if st.button("view"): 
+		st.video(path) 
+if __name__ == '__main__':
