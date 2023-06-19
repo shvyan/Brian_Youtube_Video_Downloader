3581aff4f1b4396ea80c7c27f920a599cf8463c7
shvyan <106316952+shvyan@users.noreply.github.com>
2023-06-19 18:24:56 +0300

    Update Video_Downloader.py

--- Video_Downloader.py
+++ Video_Downloader.py
@@ -22,4 +22,5 @@ def main():
 			video_object.streams.get_lowest_resolution().download()
 	if st.button("view"): 
 		st.video(path) 
-        if __name__ == '__main__':
+if __name__ == '__main__':
+	main()
